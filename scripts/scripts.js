1dc80953e7dce9c787f0e16fef18717805dc1baa
Khokhulia <a.Oleksandr.Khokhuli@Nebraska.gov>
2025-05-12 14:54:32 -0500

    js timer adjust

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -145,17 +145,13 @@ document.addEventListener("DOMContentLoaded", () => {
   headerBlock.innerHTML = `<a href="/" class="header-logo-title"><img src="../img/seal.avif" class="header-logo"><div class="header-title"><h3>Governor Jim Pillen</h3><h5>Office of the Governor</h5></div></a><div class="header-search"><input title=""  class="form-search form-control" placeholder="Search" type="search" id="edit-keys" name="keys"  size="15" maxlength="128" data-toggle="tooltip" data-original-title="Enter the terms you wish to search for."><button type="submit" value="Search" class="header-search-button"><img src="../img/search_icon.svg></button></div>`;
   header.append(headerContainer);
   headerContainer.append(headerBlock);
-
-  const isLoaded = document.querySelector('.header-sub-block')
-  console.log(isLoaded)
-  isLoaded.addEventListener('load',()=>{
+ 
+  setTimeout(() => {
     const bodyHeight =window.getComputedStyle(document.body).getPropertyValue("height")
     bodyWrapper.style.height = bodyHeight;
     document.body.append(bodyWrapper);  
-  })
-  // setTimeout(() => {
-  
-  // }, 100);
+  }, 500);
+  isLoaded.addEventListener('')
 });
 
 // sticky nav
