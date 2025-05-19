503b9f707e35854dcefe79caa2866faca4bc8367
Khokhulia <a.Oleksandr.Khokhuli@Nebraska.gov>
2025-05-19 15:50:28 -0500

    Nav hover js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -142,14 +142,14 @@ export default async function decorate(block) {
         if (isDesktop.matches) {
           const expanded = navSection.getAttribute('aria-expanded') === 'true';
           toggleAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+          navSection.setAttribute('aria-expanded', expanded)
         }
       });
-        navSection.addEventListener('mouseleave', ()=>{
+      navSection.addEventListener('mouseleave', ()=>{
           if (isDesktop.matches) {
             const expanded = navSection.getAttribute('aria-expanded') === 'false';
             toggleAllNavSections(navSections);
-            navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+            navSection.setAttribute('aria-expanded', expanded );
         }
       });
     });
