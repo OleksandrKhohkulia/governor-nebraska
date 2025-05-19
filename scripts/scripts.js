5a9e2e4b3225051d64392429fb90841b2731838d
Khokhulia <a.Oleksandr.Khokhuli@Nebraska.gov>
2025-05-19 10:24:35 -0500

    js test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -129,15 +129,15 @@ async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
-  
+
   const bodyWrapper = document.createElement("div");
   bodyWrapper.classList.add("body-wrapper");
   const bodyHeight = window
   .getComputedStyle(document.body)
   .getPropertyValue("height");
-console.log(bodyHeight)
-bodyWrapper.style.height = bodyHeight;
-document.body.append(bodyWrapper);
+  console.log(bodyHeight)
+  bodyWrapper.style.height = bodyHeight;
+  document.body.append(bodyWrapper);
 
 }
 
@@ -149,20 +149,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const headerContainer = document.createElement("div");
   const headerBlock = document.createElement("div");
   headerContainer.classList.add("header-container");
-  // const bodyWrapper = document.createElement("div");
-  // bodyWrapper.classList.add("body-wrapper");
   headerBlock.classList.add("header-sub-block");
   headerBlock.innerHTML = `<a href="/" class="header-logo-title"><img src="../img/seal.avif" class="header-logo"><div class="header-title"><h3>Governor Jim Pillen</h3><h5>Office of the Governor</h5></div></a><div class="header-search"><input title=""  class="form-search form-control" placeholder="Search" type="search" id="edit-keys" name="keys"  size="15" maxlength="128" data-toggle="tooltip" data-original-title="Enter the terms you wish to search for."><button type="submit" value="Search" class="header-search-button"><img src="../img/search_icon.svg></button></div>`;
   header.append(headerContainer);
   headerContainer.append(headerBlock);
-
-  // setTimeout(() => {
-  //   const bodyHeight = window
-  //     .getComputedStyle(document.body)
-  //     .getPropertyValue("height");
-  //   bodyWrapper.style.height = bodyHeight;
-  //   document.body.append(bodyWrapper);
-  // }, 500);
 });
 
 // sticky nav
